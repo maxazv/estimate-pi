cec518f00f40d4405cd9efbd6e601f5d0399d3df
Maximilian <65911393+maxazv@users.noreply.github.com>
2021-09-24 18:43:50 +0200

    Update estimate.cpp

--- src/estimate.cpp
+++ src/estimate.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <cmath>
 #include <stdlib.h>
 
 float approx_pi(int num){
     float in_circle = 0;
-
+    float x=0, y=0, dist=0;
     for(int i = 0; i<num; i++){
-        float x = ((double)rand() / RAND_MAX);
-        float y = ((double)rand() / RAND_MAX);
-        float dist = x*x + y*y;
+        x = ((double)rand() / RAND_MAX);
+        y = ((double)rand() / RAND_MAX);
+        dist = x*x + y*y;
 
         in_circle = dist <= 1 ? in_circle+1 : in_circle;
     }
